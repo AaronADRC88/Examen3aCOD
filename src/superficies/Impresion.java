73df22708733c580fd21db3a000498095031cf62
aferreiradominguez <aferreiradominguez@atanasoft05.danielcastelao.org>
2015-05-07 08:56:14 +0200

    elegir op y area cuadrado

--- src/superficies/Impresion.java
+++ src/superficies/Impresion.java
@@ -1,5 +1,23 @@
 package superficies;
 
+import java.util.Scanner;
+
 public class Impresion {
-static Atributos var=new Atributos();
+
+    static Atributos var = new Atributos();
+    private static Scanner sc;
+
+    public void insertKind() {
+        sc = new Scanner(System.in);
+        System.out.println("What shape would you like to find the area of? (square, circle, triangle, rectangle):");
+        var.setShapeType(sc.nextLine());
+        System.out.println("You said: " + var.getShapeType());
+    }
+    public void squareArea(){
+        System.out.println("what is the side length?");
+            var.setSideLength(sc.nextFloat()); 
+            //squareArea = sideLength * sideLength;
+            System.out.println("The area for your square: " + var.getSquareArea());
+    }
+    
 }
